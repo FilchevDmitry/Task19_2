ff6c2fe8b295334df2a8e4603c131bd62b70cf60
Filchev_Dmitry <filchev_dmitry@mail.ru>
2022-06-19 20:09:10 +0300

    Добавьте файлы проекта.

---
+++ Task19_2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include<fstream>
+#include<string>
+int main()
+{
+    std::ifstream word;
+    std::string way;
+    std::cout << "Enter the file to open : ";
+    std::cin >> way;
+    word.open(way);
+    if (!word.is_open())
+    {
+        std::cout << "The file is not open" << std::endl;
+    }
+    else 
+    {
+        std::string str;
+        while (!word.eof())
+        {
+            std::getline(word, str, '\0');
+            std::cout << str;
+        }
+    }
+    word.close();
+}
